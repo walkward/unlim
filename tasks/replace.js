58725a12073de407226b2abdadace41db37a5e3e
Walker Ward <walker.ward1@gmail.com>
2017-10-12 11:07:24 -0600

    updates

--- tasks/replace.js
+++ tasks/replace.js
@@ -8,13 +8,25 @@
 */
 
 var shell = require('shelljs');
+var Promise = require('bluebird');
 
 module.exports = function () {
 
-  // Download config files which are updated using the shopify admin
-  shell.exec('cd dist ; theme download config/settings_data.json config/settings_schema.json ; cd -');
-  shell.cat('./dist/config/settings_data.json').to('./src/config/settings_data.json');
-  shell.cat('./dist/config/settings_schema.json').to('./src/config/settings_schema.json');
+  return new Promise(function(resolve, reject) {
+    try {
+
+      var result = (function(){
+        // Download config files which are updated using the shopify admin
+        shell.exec('cd dist ; theme download config/settings_data.json config/settings_schema.json ; cd -');
+        shell.cat('./dist/config/settings_data.json').to('./src/config/settings_data.json');
+        shell.cat('./dist/config/settings_schema.json').to('./src/config/settings_schema.json');
+      })()
+      return resolve(result);
+
+    } catch (err) {
+      return reject(err);
+    }
+  })
 
   // Replace sourceMappingURL with liquid version for accurate referencing
   // shell.sed('-i', 'sourceMappingURL=theme.js.map', 'sourceMappingURL={{ "theme.js.map" | asset_url }}', './dist/assets/theme.min.js.liquid');
